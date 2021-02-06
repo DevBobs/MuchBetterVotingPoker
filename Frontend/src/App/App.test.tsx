70bb76ed1e06fda874c7ce3f9ef544709ec292b7
Til <til.wiggers@outlook.de>
2021-02-06 21:53:36 +0100

    Added 'add User action' test

--- Frontend/src/App/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Hello World', () => {
-  render(<App />);
-  const HelloWorldElement = screen.getByText('Hello World');
-  expect(HelloWorldElement).toBeInTheDocument();
-});
