63fa753060d21afe753216b80b8895f3e9b32949
Til <til.wiggers@outlook.de>
2021-02-16 21:50:34 +0100

    Initial react-router setup

--- Frontend/src/components/App/App.tsx
+++ Frontend/src/components/App/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from '../../pages/Home/Home';
 
-const App: React.FC = () => <Home />;
+const App: React.FC = () => (
+  <Router>
+    <Link to="/">Home</Link>
+    <Link to="/test">Test</Link>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/test">
+        <h1>Test page</h1>
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
